f2b034398920059703c80c0e4600459d1eae0b5f
gavinwang <gavin.wang@silkrode.com.tw>
2020-08-18 14:10:39 +0800

    refactor

--- test/test_common.py
+++ test/test_common.py
@@ -1,27 +1,29 @@
 import json
 import unittest
 
-from MongoDbTool.common import MongoBasicClient, MongoFilters
+from common import MongoBasicClient, MongoFilters
 
-
+host="cluster0.enocw.mongodb.net"
+db_name="MongoDbToolCommonUnittest"
+db_list_name="MongoDbToolCommonUnittestMainList"
 class MyTestCase(unittest.TestCase):
     def test_something_insert(self):
-        with MongoBasicClient(host="cluster0.enocw.mongodb.net",db_name="MongoDbToolCommonUnittest",db_list_name="MongoDbToolCommonUnittestMainList") as db_client:
+        with MongoBasicClient(host=host,db_name=db_name,db_list_name=db_list_name) as db_client:
             print(db_client.query(label="unitest"))
             db_client.insert(val={"label": "unitest"})
             print(db_client.query(label="unitest"))
 
     def test_something_query(self):
-        with MongoBasicClient(host="cluster0.enocw.mongodb.net",db_name="MongoDbToolCommonUnittest",db_list_name="MongoDbToolCommonUnittestMainList") as db_client:
+        with MongoBasicClient(host=host,db_name=db_name,db_list_name=db_list_name) as db_client:
             print(db_client.query(label="unitest"))
 
     def test_something_enter_exit(self):
-        with MongoBasicClient(host="cluster0.enocw.mongodb.net",db_name="MongoDbToolCommonUnittest",db_list_name="MongoDbToolCommonUnittestMainList") as db_client:
+        with MongoBasicClient(host=host,db_name=db_name,db_list_name=db_list_name) as db_client:
             db_client.insert(val={"label": "unitest"})
             print(db_client.query(label="unitest"))
 
     def test_something_query_in(self):
-        with MongoBasicClient(host="cluster0.enocw.mongodb.net",db_name="fbbot_like_pic",db_list_name="user_like") as db_client:
+        with MongoBasicClient(host=host,db_name="fbbot_like_pic",db_list_name="user_like") as db_client:
             print(db_client.query_in(shortcode=["B0yUI-njUwK", "B0u5pqjn9mm", "B0yUD21nh4M"]))
 
     def test_MongoFilters(self):
@@ -65,7 +67,7 @@ class MyTestCase(unittest.TestCase):
         fs.add_filter_in(colume="shortcode", val=["B0yUI-njUwK","B","C"])\
             .add_filter_in(colume="label2", val=["C","D","E"])\
             .add_filter_equal(colume="label3", val="hi")
-        with MongoBasicClient(host="cluster0.enocw.mongodb.net", db_name="fbbot_like_pic",
+        with MongoBasicClient(host=host, db_name="fbbot_like_pic",
                               db_list_name="user_like") as db_client:
             print(db_client.query_by_filters(filters=fs))
 
@@ -74,7 +76,7 @@ class MyTestCase(unittest.TestCase):
         fs = MongoFilters()
         fs.add_filter_in(colume="shortcode", val=["B0yUI-njUwK","B","C"])\
             .or_filters(MongoFilters().add_filter_equal(colume="label3", val="hi"))
-        with MongoBasicClient(host="cluster0.enocw.mongodb.net", db_name="fbbot_like_pic",
+        with MongoBasicClient(host=host, db_name="fbbot_like_pic",
                               db_list_name="user_like") as db_client:
             print(db_client.query_by_filters(filters=fs))
 
@@ -83,7 +85,7 @@ class MyTestCase(unittest.TestCase):
         fs = MongoFilters()
         fs.add_filter_in(colume="shortcode", val=["B0yUI-njUwK","B","C"])\
             .or_filters(MongoFilters().add_filter_equal(colume="label3", val="hi"))
-        with MongoBasicClient(host="cluster0.enocw.mongodb.net", db_name="fbbot_like_pic",
+        with MongoBasicClient(host=host, db_name="fbbot_like_pic",
                               db_list_name="user_like") as db_client:
             result = db_client.query_by_filters(filters=MongoFilters()
                                                 .add_filter_in(colume="shortcode", val=["B0yUI-njUwK","B","C"])
@@ -98,7 +100,7 @@ class MyTestCase(unittest.TestCase):
         fs = MongoFilters()
         fs.add_filter_in(colume="shortcode", val=["B0yUI-njUwK","B","C"])\
             .or_filters(MongoFilters().add_filter_equal(colume="label3", val="hi"))
-        with MongoBasicClient(host="cluster0.enocw.mongodb.net", db_name="fbbot_like_pic",
+        with MongoBasicClient(host=host, db_name="fbbot_like_pic",
                               db_list_name="user_like") as db_client:
             result = db_client.query_by_filters(filters=MongoFilters()
                                                   .add_filter_in(colume="shortcode", val=["B0yUI-njUwK", "B", "C"])
